a057ec1189f63f99acb371b724cf4cc89ecc16ea
山本涼 <hekimosath@gmail.com>
2018-07-28 13:10:34 +0900

    brainStormingTool developing start

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
+  get 'friends/room'
   resources :friends
   get 'users/index'
   get 'users/show'
   get 'friends', to: 'friends#index'
   root :to => 'friends#index'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
